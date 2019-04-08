38185d8c48b95b7781b29e3bca2346abc0dcc7e1
JPDent <justind@Justins-MacBook-Pro-3.local>
2019-04-08 16:56:15 -0700

    finished everything except winCounter and responsive layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,136 @@
 
+$(function () {
 
 
 
 
-// $("#gemBlack").click(adder());
 
 
-// *****************************************
-
-//Establish Variables
-// var matchNum = randNum;
-// var wins = 0;
-// var losses = 0;
+    // $("#gemBlack").click(adder());
 
 
-//on page load [function reset()]
+    // *****************************************
 
-// var gemArray = []
-// var totalScore = 0;
+    //Establish Variables
+    // var matchNum = randNum;
+    var wins = 0;
+    var losses = 0;
+    var gemArray = []
+    var totalScore = "";
+    var blackAmount = "";
+    var blueAmount = "";
+    var whiteAmount = "";
+    var yellowAmount = "";
+
+    // -----ON PAGE LOAD [function reset()]------
+
+    function reset() {
+        gemArray = []
+        totalScore = 0;
+
+        blackAmount = 0;
+        blueAmount = 0;
+        whiteAmount = 0;
+        yellowAmount = 0;
 
 
-// 1) pick Random Number to Match
-//          randNum = Math.floor(Math.random() * 120) + 1;
-//          $("#matchNum").html(randNum);
+        $("#wins").html(wins);
+        $("#losses").html(losses);
 
-// 2) pick Random Amounts for: gemArray
-//      pickBlack()
-//          blueAmount= Math.floor(math.random) *12) +1
-//          push to gemArray[i]
-//      pickblue()
-//          blueAmount= Math.floor(math.random) *12) +1
-//          push to gemArray[i]
-//      pickWhite()
-//          blueAmount= Math.floor(math.random) *12) +1
-//          push to gemArray[i]
-//      pickYellow()
-//          blueAmount= Math.floor(math.random) *12) +1
-//          push to gemArray[i]
 
-// 3) 
-//      #gemBlack       $("#gemBlack").click(function () {
-//                      gemArray[0] + totalScore;
-//                      $("#totalScore").html(totalScore);
-//                      });
-//      #gemBlue       $("#gemBlue").click(function () {
-//                      gemArray[1] + totalScore;
-//                      $("#totalScore").html(totalScore);
-//                      });
-//      #gemWhite       $("#gemWhite").click(function () {
-//                      gemArray[2] + totalScore;
-//                      $("#totalScore").html(totalScore);
-//                      });
-//      #gemYellow      $("#gemYellow").click(function () {
-//                      gemArray[3] + totalScore;
-//                      $("#totalScore").html(totalScore);
-//                      });
 
-//      $("#totalScore").html(totalScore);
+        // 1) pick Random Number to Match
+        randNum = Math.floor(Math.random() * 120) + 1;
+        $("#matchNum").html(randNum);
 
-// 4)   Count Wins/Losses & reset game
-//
-//      if (totalScore === matchNum) {
-//      wins++;
-//      reset()
-//      } else if (totalScore > matchNum) {
-//      losses++
-//      reset()
-//      }; 
+        // 2) pick Random Amounts for: gemArray
+        function pickBlack() {
+            blackAmount = Math.floor((Math.random() * 12) + 1);
+            gemArray.push(blackAmount);
+            console.log(blackAmount);
+            console.log(gemArray);
+        };
+        pickBlack();
 
+        function pickBlue() {
+            blueAmount = Math.floor((Math.random() * 12) + 1);
+            gemArray.push(blueAmount);
+            console.log(blueAmount);
+            console.log(gemArray);
+        };
+        pickBlue();
+        function pickWhite() {
+            whiteAmount = Math.floor((Math.random() * 12) + 1);
+            gemArray.push(whiteAmount);
+            console.log(whiteAmount);
+            console.log(gemArray);
+        };
+        pickWhite();
+        function pickYellow() {
+            yellowAmount = Math.floor((Math.random() * 12) + 1);
+            gemArray.push(yellowAmount);
+            console.log(yellowAmount);
+            console.log(gemArray);
+        };
+        pickYellow();
+    };
+
+    reset();
+    // -----RESET COMPLETE------
+
+
+    function playGame() {
+
+
+        // 3)   BUTTON CLICKS (ADD TO TOTAL SCORE)
+
+        //      #gemBlack       
+        $("#gemBlack").click(function () {
+            gemArray[0]
+            var blackScore = gemArray[0];
+            console.log(blackScore);
+            totalScore = (totalScore + blackScore);
+            $("#totalScore").html(totalScore);
+        });
+        //      #gemBlue
+        $("#gemBlue").click(function () {
+            gemArray[1]
+            var blueScore = gemArray[1];
+            console.log(blueScore);
+            totalScore = (totalScore + blueScore);
+            $("#totalScore").html(totalScore);
+        });
+        //      #gemWhite
+        $("#gemWhite").click(function () {
+            gemArray[2]
+            var whiteScore = gemArray[2];
+            console.log(whiteScore);
+            totalScore = (totalScore + whiteScore);
+            $("#totalScore").html(totalScore);
+        });
+        //      #gemYellow
+        $("#gemYellow").click(function () {
+            gemArray[3]
+            var yellowScore = gemArray[3];
+            console.log(yellowScore);
+            totalScore = (totalScore + yellowScore);
+            $("#totalScore").html(totalScore);
+        });
+
+        // 4)   Count Wins/Losses & reset game
+        function checkWin() {
+            if (totalScore === randNum) {
+                wins++;
+                reset();
+            } else if (totalScore > randNum) {
+                losses++;
+                reset();
+            };
+        };
+        checkWin();
+    };
+    playGame();
+
+});
+
+//-------END OF PLAY GAME-----
